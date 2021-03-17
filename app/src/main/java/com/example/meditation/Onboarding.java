2e102ee158278698d755796c83034617510c2ff5
ii <nagaev.egor@mail.ru>
2021-03-17 16:31:37 +0500

    med2

--- app/src/main/java/com/example/meditation/Onboarding.java
+++ app/src/main/java/com/example/meditation/Onboarding.java
@@ -3,15 +3,26 @@ package com.example.meditation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
 public class Onboarding extends AppCompatActivity {
-
+ String token;
+ SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_onboarding);
+        sharedPreferences =getSharedPreferences("main",MODE_PRIVATE);
+
+        if(sharedPreferences.getString("token",null)!=null) {
+            Intent log = new Intent(Onboarding.this, Menu.class);
+            startActivity(log);
+        }
+        else {
+
+            setContentView(R.layout.activity_onboarding);
+        }
     }
 
     public void login(View view) {
--- app/src/main/java/com/example/meditation/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/meditation/ui/home/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.example.meditation.ui.home;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.SurfaceControl;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -12,11 +14,15 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.meditation.MenuAct;
 import com.example.meditation.Network.Emot;
 import com.example.meditation.Network.Feelings;
 import com.example.meditation.Network.MyRetrofit;
@@ -24,6 +30,7 @@ import com.example.meditation.Network.RetrofitCalls;
 import com.example.meditation.R;
 import com.example.meditation.Recyclers.EmtionRecycler;
 import com.example.meditation.Recyclers.FeelingsRecycler;
+import com.example.meditation.ui.profile.ProfileFragment;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -32,7 +39,7 @@ import retrofit2.Retrofit;
 
 public class HomeFragment extends Fragment {
 String avatar,name,token;
-ImageView av;
+ImageView av,menu;
 TextView user;
 RecyclerView emotions,state;
 SharedPreferences sharedPreferences;
@@ -42,6 +49,7 @@ SharedPreferences sharedPreferences;
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         state = root.findViewById(R.id.log);
         av = root.findViewById(R.id.imageavatar);
+        menu = root.findViewById(R.id.menu);
         emotions = root.findViewById(R.id.emotion);
         user = root.findViewById(R.id.textView5);
         Retrofit ret = MyRetrofit.getretofit();
@@ -50,6 +58,19 @@ SharedPreferences sharedPreferences;
         avatar = sharedPreferences.getString("avatar","");
         name = sharedPreferences.getString("name","Loh");
         user.setText("С возвращением," +name +"!");
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent men = new Intent(getActivity(), MenuAct.class);
+                startActivity(men);
+            }
+        });
+        av.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(root).navigate(R.id.action_navigation_home_to_navigation_profile2);
+            }
+        });
         Glide.with(root).load(avatar).circleCrop().into(av);
         RetrofitCalls rcall = ret.create(RetrofitCalls.class);
         Call<Feelings> emotio = rcall.feelings();
@@ -81,6 +102,7 @@ SharedPreferences sharedPreferences;
 
             }
         });
+
 
 
 
